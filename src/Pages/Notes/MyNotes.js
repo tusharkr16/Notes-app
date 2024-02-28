6fdd270d0ba55b74b347b30b5c3e6cfe1668a304
tusharkr16 <tusharkumar9871@gmail.com>
2024-02-29 01:09:15 +0530

    hashing the password

--- src/Pages/Notes/MyNotes.js
+++ src/Pages/Notes/MyNotes.js
@@ -1,42 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Screen from '../Screen'
 import { Link } from 'react-router-dom'
 import { Badge, Button, Card } from 'react-bootstrap'
+import axios from 'axios'
 
-const notes = [
-    {
-        _id: "1",
-        title: "Day 1 of college",
-        content:
-            "I made a few new friends and introduced myself to a lot of new teachers.",
-        category: "College",
-    },
-    {
-        _id: "2",
-        title: "Learned some Node JS",
-        content: "Learned how to create a server in node JS and my first API",
-        category: "Learning",
-    },
-    {
-        _id: "3",
-        title: "Watched some Anime",
-        content: "Finished 2 seasons of Attack on Titan and My Hero academia.",
-        category: "Entertainment",
-    },
-    {
-        _id: 4,
-        title: "Started React JS",
-        content:
-            "Made my first App in React JS, feels awesome to learn something new. I aim to be a full stack dev someday",
-        category: "Learning",
-    },
-];
+
 
 const MyNotes = () => {
+    const [notes, setNotes] = useState([]);
 
     const deleteHandler = (id) => {
         window.alert('are u sure?')
     }
+
+    const fetchNotes = async () => {
+        try {
+            const { data } = await axios.get('/api/notes');
+            setNotes(data);
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    console.log(notes)
+    useEffect(() => {
+        fetchNotes();
+    }, [])
     return (
         <Screen title={'Welcome Tushar kumar'}>
             <Link to='createNode'>
@@ -45,7 +36,7 @@ const MyNotes = () => {
                 </Button>
             </Link>
             {notes.map((note) => (
-                <Card style={{ margin: 10 }}>
+                <Card style={{ margin: 10 }} key={note._id}>
                     <Card.Header style={{ display: 'flex', }}>
                         <span style={{
                             color: 'black',
